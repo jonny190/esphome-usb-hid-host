656a7159ea1db1d8501723bdfec0a92aa31ee0b7
Jonny Davey <Jonny190@gmail.com>
2025-08-08 10:44:24 +0100

    TestFix

--- components/usb_hid_keyboard/text_sensor/usb_hid_keyboard_text_sensor.cpp
+++ components/usb_hid_keyboard/text_sensor/usb_hid_keyboard_text_sensor.cpp
@@ -1,50 +1,8 @@
 #include "esphome/components/usb_hid_keyboard/usb_hid_keyboard.h"
 #include "esphome/core/log.h"
 
-static const char *const TAG = "usb_hid_keyboard";
-
 namespace esphome {
 namespace usb_hid_keyboard {
-
-static UsbHidKeyboardManager *s_instance = nullptr;
-
-UsbHidKeyboardManager *UsbHidKeyboardManager::instance() { return s_instance; }
-
-void UsbHidKeyboardManager::setup() {
-  ESP_LOGI(TAG, "Setting up USB HID Keyboard Manager");
-  s_instance = this;
-  init_usb_host_();
-}
-
-void UsbHidKeyboardManager::loop() {
-  poll_usb_();
-
-  while (!key_queue_.empty()) {
-    auto k = key_queue_.front();
-    key_queue_.pop();
-
-    if (last_key_sensor_ != nullptr) last_key_sensor_->publish_state(k);
-    ESP_LOGV(TAG, "Key: %s", k.c_str());
-
-    // Pulse all registered binary sensors
-    for (auto *bs : binary_sensors_) {
-      if (bs != nullptr) bs->on_key_pulse();
-    }
-  }
-}
-
-void UsbHidKeyboardManager::enqueue_key(const std::string &key) {
-  key_queue_.push(key);
-}
-
-void UsbHidKeyboardManager::init_usb_host_() {
-  // TODO: IDF USB host + HID init here
-  ESP_LOGI(TAG, "USB host init (stub)");
-}
-
-void UsbHidKeyboardManager::poll_usb_() {
-  // TODO: Handle HID reports → decode → enqueue_key("A");
-}
-
+// Nothing needed here for the text sensor itself; the manager publishes into it.
 }  // namespace usb_hid_keyboard
 }  // namespace esphome
