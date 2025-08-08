7d2ea70eaba6b4662cc4e273131cd30512b57a2d
Jonny Davey <Jonny190@gmail.com>
2025-08-08 10:50:07 +0100

    use ESPHome’s HAL helper

--- components/usb_hid_keyboard/binary_sensor/usb_hid_keyboard_binary_sensor.cpp
+++ components/usb_hid_keyboard/binary_sensor/usb_hid_keyboard_binary_sensor.cpp
@@ -1,5 +1,6 @@
 #include "usb_hid_keyboard_binary_sensor.h"
 #include "esphome/core/helpers.h"
+#include "esphome/core/hal.h"   // <-- add this
 
 namespace esphome {
 namespace usb_hid_keyboard {
@@ -7,7 +8,7 @@ namespace usb_hid_keyboard {
 void UsbHidKeyboardBinarySensor::on_key_pulse() {
   this->publish_state(true);
   this->pending_pulse_ = true;
-  this->pulse_start_ms_ = millis();
+  if ((esphome::millis() - pulse_start_ms_) >= pulse_len_ms_) {
 }
 
 void UsbHidKeyboardBinarySensor::loop() {
